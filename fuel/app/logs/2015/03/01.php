00420cce7716992acdd02eac23e52817ed767fdb
User Goldkeito <goldkeito@www3542.sakura.ne.jp>
2015-03-01 20:41:59 +0900

    fuel add.

---
+++ fuel/app/logs/2015/03/01.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2015-03-01 20:41:21 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
